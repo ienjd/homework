9924453c5361031ca475eba9d643cd4cb5fb371e
Vladislav Shutov <coldhobbit31@ya.ru>
2024-09-19 15:54:11 +0300

    Домашка 1
    навигатор

---
+++ 4-navigator/4-navigator.js
@@ -0,0 +1,13 @@
+let positionLat = 89;
+let positionLong = 79;
+
+let addressLat = 96;
+let addressLong = 99;
+
+let distanceLat = Math.abs(positionLat - addressLat);
+let distanceLong = Math.abs(positionLong - addressLong);
+
+let distance = Math.sqrt((distanceLat ** 2) + (distanceLong ** 2));
+
+console.log(distance.toFixed(2));
+
