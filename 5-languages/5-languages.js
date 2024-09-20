9309301e61bf9a272a7cb0edf882534855cbbbde
Vladislav Shutov <coldhobbit31@ya.ru>
2024-09-20 10:32:56 +0300

    Вторая домашка
    Языки

---
+++ 5-languages/5-languages.js
@@ -0,0 +1,23 @@
+let lang = prompt("Выберите язык");
+
+switch(lang) {
+
+    case "ru":
+        console.log("Хорошего дня!")
+            break;
+    
+    case "en":
+        console.log("Have a nice day!")
+            break;
+
+    case "de":
+        console.log("Einen schönen Tag!")
+            break;
+
+    case "fr":
+        console.log("Bonne journée!")
+             break;
+}
+
+
+
