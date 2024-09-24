d0b515426455e3c9557a1b7de6b10963e03a8857
Vladislav Shutov <coldhobbit31@ya.ru>
2024-09-24 17:51:40 +0300

    после правок

--- 6-driver/6-driver.js
+++ 6-driver/6-driver.js
@@ -1,10 +1,10 @@
 const hasLicense = true;
 const age = 18;
-const isDrink = true;
+const isDrink = false;
 
 const canDrive = hasLicense && age >= 18 && !isDrink;
 
-console.log(`Водитель ${canDrive ? "может" : "не может"} сесть за руль`)
+console.log(`Водитель ${canDrive ? "может" : "не может"} сесть за руль`);
 
 
 
